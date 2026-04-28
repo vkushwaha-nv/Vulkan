d7b5a03c2997a953ad97c3ac898e75027da4b3d0
Vikram Kushwaha <vkushwaha@nvidia.com>
2026-04-28 19:37:42 -0400

    Add compile.bat

---
+++ shaders/glsl/raytracingVK/compile.bat
@@ -0,0 +1,4 @@
+glslangValidator --target-env spirv1.4 -V raygen.rgen -o raygen.rgen.spv
+glslangValidator --target-env spirv1.4 -V closesthit.rchit -o closesthit.rchit.spv
+glslangValidator --target-env spirv1.4 -V miss.rmiss -o miss.rmiss.spv
+glslangValidator --target-env spirv1.4 -V anyhit.rahit -o anyhit.rahit.spv
